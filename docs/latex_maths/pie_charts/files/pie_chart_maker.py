3d3a8675117ce4b117735c21ae347dcc846ef129
gmc-code <gmccarthy@tpg.com.au>
2023-05-10 08:04:48 +1000

    up

---
+++ docs/latex_maths/pie_charts/files/pie_chart_maker.py
@@ -0,0 +1,118 @@
+# standard numpy and matplotlib library imports
+import numpy as np
+from pathlib import Path
+import subprocess
+from tkinter import filedialog
+import time
+import magick_pdf_to_png
+
+currfile_dir = Path(__file__).parent
+tex_template_path = currfile_dir / "pie_chart_template.tex"
+
+
+def convert_to_pdf(tex_path, currfile_dir, aux_path):
+    result = subprocess.run(
+        [
+            "pdfLaTeX",
+            tex_path,
+            "-output-directory",
+            currfile_dir,
+            "-aux-directory",
+            aux_path,
+        ],
+        stdout=subprocess.PIPE,
+    )
+
+
+def get_list_from_str(data_string):
+    if ", " in data_string:
+        data_list = data_string.split(", ")
+    elif "," in data_string:
+        data_list = data_string.split(",")
+    else:
+        data_list = data_string.split(" ")
+    return data_list
+
+
+def get_list_nums_from_str(num_string_list):
+    if "." in num_string_list:
+        num_list = [float(n) for n in num_string_list]
+    else:
+        num_list = [int(n) for n in num_string_list]
+    return num_list
+
+
+def get_file_data(filename):
+    # open the text file and read the numbers
+    with open(filename) as f:
+        # read the first line and store it in a variable
+        plot_title = f.readline().strip()
+        # read the second line and store it in a variable
+        numbers_string = f.readline().strip()
+        numbers_strs = get_list_from_str(numbers_string)
+        # read the third line and store it in a variable
+        numbers_labels = f.readline().strip()
+        numbers_labels = get_list_from_str(numbers_labels)
+        # process numbers
+        numbers_loop_max = str(len(numbers_strs) - 1)
+        numbers_nums = get_list_nums_from_str(numbers_strs)
+        xmax = str(sum(numbers_nums))
+    return plot_title, numbers_strs, numbers_labels, numbers_loop_max, xmax
+
+
+def main():
+    data_filename = filedialog.askopenfilename(initialdir=Path(currfile_dir))
+    if data_filename == "":
+        print("Exited, by clicking Cancel")
+        return
+    plot_title, numbers_string, numbers_labels, numbers_loop_max, xmax = get_file_data(
+        data_filename
+    )
+    # print(plot_title, numbers_string, numbers_labels, numbers_loop_max)
+
+    # Create a Path object from the file path
+    path_obj = Path(data_filename)
+
+    # Get the file name from the Path object using the name attribute
+    filename = path_obj.stem
+    # filename = input("Enter the base filename to be added to the prefix dp_: \n")
+    # if not filename:
+    #     filename = "dp_1st"
+    # set names of files that are made
+    tex_output_path = currfile_dir / f"{filename}.tex"
+    pdf_path = currfile_dir / f"{filename}.pdf"
+    png_path = currfile_dir / f"{filename}.png"
+    aux_path = currfile_dir / "temp"
+    # Read in the LaTeX template file
+    with open(tex_template_path, "r") as infile:
+        tex_template_txt = infile.read()
+
+    # Replace the placeholders in the LaTeX template
+
+    tex_template_txt = tex_template_txt.replace("<<plot_title>>", plot_title)
+    tex_template_txt = tex_template_txt.replace(
+        "<<numbers_loop_max>>", numbers_loop_max
+    )
+    tex_template_txt = tex_template_txt.replace("<<xmax>>", xmax)
+    tex_template_txt = tex_template_txt.replace("<<numbers_labels>>", numbers_labels)
+    tex_template_txt = tex_template_txt.replace("<<numbers_string>>", numbers_string)
+
+    # Write the question tex to an output file
+    with open(tex_output_path, "w") as outfile:
+        outfile.write(tex_template_txt)
+
+    # Wait for the files to be created
+    time.sleep(1)
+    # Convert the LaTeX files to PDFs
+    convert_to_pdf(tex_output_path, currfile_dir, aux_path)
+
+    # Wait for the files to be created
+    time.sleep(1)
+    # Convert the PDFs to PNGs
+    magick_pdf_to_png.convert_pdf_to_png(pdf_path, png_path)
+
+
+if __name__ == "__main__":
+    print("starting")
+    main()
+    print("finished")
